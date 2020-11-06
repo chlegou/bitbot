ecdffc14223fa4c7b5224682419f3541f00525f9
chlegou <nicolastsue@gmail.com>
2020-11-06 09:12:39 +0100

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,205 @@
+$(window).on('load', function(){
+
+	"use strict";
+ 
+	
+	/* ========================================================== */
+	/*   Owl Carousel                                             */
+	/* ========================================================== */
+	
+	$('#owl1').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	})
+
+	$('#owl2').owlCarousel({
+	    center:true,
+	    autoplay:true,
+	    loop:true,
+	    margin:40,
+	    nav:false,
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	            nav:false
+	        },
+	        600:{
+	            items:2,
+	            nav:false
+	        },
+	        1000:{
+	            items:4,
+	            nav:false,
+	            loop:true
+	        }
+	    }
+	})
+ 
+	/* ========================================================== */
+	/*   Navigation Background Color                              */
+	/* ========================================================== */
+	
+	$(window).on('scroll', function() {
+		if($(this).scrollTop() > 100) {
+			$('.navbar-fixed-top').addClass('opaque');
+		} else {
+			$('.navbar-fixed-top').removeClass('opaque');
+		}
+	});
+ 
+	
+	/* ========================================================== */
+	/*   Hide Responsive Navigation On-Click                      */
+	/* ========================================================== */
+	
+	  $(".navbar-nav li a").on('click', function(event) {
+	    $(".navbar-collapse").collapse('hide');
+	  });
+
+	
+	/* ========================================================== */
+	/*   Navigation Color                                         */
+	/* ========================================================== */
+	
+	$('#navbar-collapse-02').onePageNav({
+		filter: ':not(.external)'
+	});
+
+
+	/* ========================================================== */
+	/*   SmoothScroll                                             */
+	/* ========================================================== */
+	
+	$(".nav li a:not(.discover-btn), a.scrool").on('click', function(e) {
+		
+		var full_url = this.href;
+		var parts = full_url.split("#");
+		var trgt = parts[1];
+		var target_offset = $("#"+trgt).offset();
+		var target_top = target_offset.top;
+		
+		$('html,body').animate({scrollTop:target_top -70}, 1000);
+			return false;
+		
+	});
+
+
+	/* ========================================================== */
+	/*   Newsletter                                               */
+	/* ========================================================== */
+	
+	$('.newsletter_box .newsletter_form').each( function(){
+		var form = $(this);
+		//form.validate();
+		form.submit(function(e) {
+			if (!e.isDefaultPrevented()) {
+				jQuery.post(this.action,{
+					'email':$('input[name="nf_email"]').val(),
+				},function(data){
+					form.fadeOut('fast', function() {
+						$(this).siblings('p.newsletter_success_box').show();
+					});
+				});
+				e.preventDefault();
+			}
+		});
+	});	
+	
+
+	/* ========================================================== */
+	/*   Register                                                 */
+	/* ========================================================== */
+	
+	$('#register-form').each( function(){
+		var form = $(this);
+		//form.validate();
+		form.submit(function(e) {
+			if (!e.isDefaultPrevented()) {
+				jQuery.post(this.action,{
+					'names':$('input[name="register_names"]').val(),
+					'phone':$('input[name="register_phone"]').val(),
+					'email':$('input[name="register_email"]').val(),
+					'ticket':$('select[name="register_ticket"]').val(),
+				},function(data){
+					form.fadeOut('fast', function() {
+						$(this).siblings('p.register_success_box').show();
+					});
+				});
+				e.preventDefault();
+			}
+		});
+	})
+	
+	
+	/* ========================================================== */
+	/*   Contact                                                  */
+	/* ========================================================== */
+	$('#contact-form').each( function(){
+		var form = $(this);
+		//form.validate();
+		form.submit(function(e) {
+			if (!e.isDefaultPrevented()) {
+				jQuery.post(this.action,{
+					'names':$('input[name="contact_names"]').val(),
+					'subject':$('input[name="contact_subject"]').val(),
+					'email':$('input[name="contact_email"]').val(),
+					'phone':$('input[name="contact_phone"]').val(),
+					'message':$('textarea[name="contact_message"]').val(),
+				},function(data){
+					form.fadeOut('fast', function() {
+						$(this).siblings('p').show();
+					});
+				});
+				e.preventDefault();
+			}
+		});
+	})
+
+	
+});
+
+
+	/* ========================================================== */
+	/*   Popup-Gallery                                            */
+	/* ========================================================== */
+	$('.popup-gallery').find('a.popup1').magnificPopup({
+		type: 'image',
+		gallery: {
+		  enabled:true
+		}
+	}); 
+	
+	$('.popup-gallery').find('a.popup2').magnificPopup({
+		type: 'image',
+		gallery: {
+		  enabled:true
+		}
+	}); 
+ 
+	$('.popup-gallery').find('a.popup3').magnificPopup({
+		type: 'image',
+		gallery: {
+		  enabled:true
+		}
+	}); 
+ 
+	$('.popup-gallery').find('a.popup4').magnificPopup({
+		type: 'iframe',
+		gallery: {
+		  enabled:false
+		}
+	});  
+ 
